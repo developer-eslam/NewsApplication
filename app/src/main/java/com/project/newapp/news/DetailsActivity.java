0e0496e8a9bc1ac4b8bdc27be0853494e0dd8ddf
developer-eslam <developer-eslam@users.noreply.github.com>
2017-03-20 23:32:20 +0200

    Update DetailsActivity.java

--- app/src/main/java/com/project/newapp/news/DetailsActivity.java
+++ app/src/main/java/com/project/newapp/news/DetailsActivity.java
@@ -1,11 +1,15 @@
 package com.project.newapp.news;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -21,8 +25,13 @@ import adapter.DetailsRecyclerviewAdapter;
 import model.articles;
 import model.sources;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -34,6 +43,7 @@ public class DetailsActivity extends AppCompatActivity {
     NetworkController networkController;
 
 
+    articles articles= new articles();
 
 
 
@@ -41,9 +51,6 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-
-
-
 
         requestQueue = Volley.newRequestQueue(this);
             recyclerView = (RecyclerView) findViewById(R.id.rcy);
@@ -56,56 +63,60 @@ public class DetailsActivity extends AppCompatActivity {
 
 
 
-        articles articles = new articles();
+        String apiKey="c6ba249db52c44518722fe4796b6ea1d";
 
-         JSONObject params = new JSONObject();
-
+        JSONObject jsonObject = new JSONObject();
         try {
-
-             String a = params.getString("sources");
-            String url = "https://newsapi.org/v1/articles?source="+a+"&sortBy=top&apiKey=c6ba249db52c44518722fe4796b6ea1d";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest( url
-         ,params,new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-
-                    try {
+                 String source = jsonObject.getString("source");
 
 
-                        JSONArray jsonArrayarticles = response.getJSONArray("articles");
-                        for (i = 0; i < jsonArrayarticles.length(); i++) {
-                            JSONObject jsonObject = jsonArrayarticles.getJSONObject(i);
-                            String title = jsonObject.getString("title");
-                            String author = jsonObject.getString("author");
-                            String urlToImage = jsonObject.getString("urlToImage");
-                            model.articles detailsarticles = new articles(title, author, urlToImage);
+                 articles articles = new articles(source);
 
 
-                            articlesList.add(detailsarticles);
+                String url = "https://newsapi.org/v1/articles?source=" +articles.getSources()+ "&apiKey=" + apiKey;
 
+                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url
+                        , new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+                            JSONArray jsonArrayarticles = response.getJSONArray("articles");
+                            for (i = 0; i < jsonArrayarticles.length(); i++) {
+                                JSONObject jsonObject = jsonArrayarticles.getJSONObject(i);
+                                String title = jsonObject.getString("title");
+                                String author = jsonObject.getString("author");
+                                String urlToImage = jsonObject.getString("urlToImage");
+                                model.articles detailsarticles = new articles(title, author, urlToImage);
+
+
+                                articlesList.add(detailsarticles);
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } finally {
+                            detailsRecyclerviewAdapter.notifyItemChanged(i);
                         }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    } finally {
-                        detailsRecyclerviewAdapter.notifyItemChanged(i);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
                     }
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
 
-                }
-            });
+                );
 
-            requestQueue.add(jsonObjectRequest);
+                requestQueue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-
-        }catch (JSONException e){
 
         }
 
 
-
-    }
 }
